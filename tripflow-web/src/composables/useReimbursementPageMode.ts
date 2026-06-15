9362844dbc9682e23150e1c0f26b4a58804295c4
Jenrimark <2303532728@qq.com>
2026-06-15 18:37:09 +0800

    feat(web): 作废状态报销单自动进入查看模式

--- tripflow-web/src/composables/useReimbursementPageMode.ts
+++ tripflow-web/src/composables/useReimbursementPageMode.ts
@@ -1,10 +1,16 @@
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import { useReimbursementStore } from '@/stores/reimbursementStore'
+import { DocumentStatus } from '@/types/reimbursement'
 
 export function useReimbursementPageMode() {
   const route = useRoute()
+  const store = useReimbursementStore()
 
-  const isViewMode = computed(() => route.name === 'reimbursement-detail')
+  const isViewMode = computed(() => {
+    if (route.name === 'reimbursement-detail') return true
+    return store.currentReimbursement?.status === DocumentStatus.VOIDED
+  })
 
   return { isViewMode }
 }
